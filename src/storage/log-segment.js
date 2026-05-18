623d82e3a61276ff1bfeef33e33e809037093308
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-18 13:11:11 +0000

    added an isfull method

--- src/storage/log-segment.js
+++ src/storage/log-segment.js
@@ -2,13 +2,22 @@ const fs = require('fs');
 const path = require('path');
 
 class LogSegment{
-    constructor(topicDir, baseOffset, maxBytes=10*1024*1024){
+    constructor(topicDir, baseOffset, maxBytes=10*1024*1024, mode = 'write'){
         this.baseOffset = baseOffset;
         this.maxBytes = maxBytes;
         
         const fileNameBase = baseOffset.toString().padStart(20, '0');
         this.logPath = path.join(topicDir, `${fileNameBase}.log`);
         this.indexPath = path.join(topicDir, `${fileNameBase}.index`);
+
+        const fileFlag = (mode==='write')?'a+':'r';
+        
+        const stats = fs.statSync(this.logPath);
+        this.currentBytes= stats.size;
+    }
+
+    isFull(){
+        return (this.currentBytes >= this.maxBytes);
     }
 }
 
