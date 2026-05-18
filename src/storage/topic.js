58b8f8d5e6412eefa93cae8e3f3163c94e0bb085
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-18 11:16:47 +0000

    structure for topic class created

---
+++ src/storage/topic.js
@@ -0,0 +1,27 @@
+const fs = require('fs');
+const LogSegment = require('./log-segment');
+const path = require('path');
+
+//check in the api if the topic directory is already existing, so topicDir will essentially be passed form the api if it is not duplicated.
+
+class Topic{
+    constructor(userId, topicName, topicDir){
+        this.name = topicName;
+        this.user = userId;
+        this.topicDir = topicDir;
+        this.nextOffset = 0n;
+        this.activeSegment = new LogSegment(this.topicDir, this.nextOffset);
+    }
+
+    changeActiveSegment(segment){ // segment will be an object here, lead with apis which trigger appending data, incase new segment gets made
+        this.activeSegment = segment;
+    }
+
+    getNextOffset(){
+        return this.nextOffset;
+    }
+
+    incrementNextOffset(){
+        this.nextOffset++;
+    }
+}
