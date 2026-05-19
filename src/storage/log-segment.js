92d3b703d05cf982a853bae332beec74f40422da
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-19 10:41:00 +0000

    added file descriptors as an optimization

--- src/storage/log-segment.js
+++ src/storage/log-segment.js
@@ -5,19 +5,40 @@ class LogSegment{
     constructor(topicDir, baseOffset, maxBytes=10*1024*1024, mode = 'write'){
         this.baseOffset = baseOffset;
         this.maxBytes = maxBytes;
+        this.mode = mode;
         
         const fileNameBase = baseOffset.toString().padStart(20, '0');
         this.logPath = path.join(topicDir, `${fileNameBase}.log`);
         this.indexPath = path.join(topicDir, `${fileNameBase}.index`);
 
-        const fileFlag = (mode==='write')?'a+':'r';
+        if(!fs.existsSync(this.logPath)){
+            fs.writeFileSync(this.logPath, '');
+        }
+
+        if(!fs.existsSync(this.indexPath)){
+            fs.writeFileSync(this.indexPath, '');
+        }
         
-        const stats = fs.statSync(this.logPath);
+        this.fileFlag = (mode==='write')?'a+':'r';
+
+        try{
+            this.logFd = fs.openSync(this.logPath, this.fileFlag);
+            this.indexFd = fs.openSync(this.indexPath, this.fileFlag);
+        } catch(error){
+            throw error;
+        }
+
+        const stats = fs.fstatSync(this.logFd);
         this.currentBytes= stats.size;
     }
 
     isFull(){
         return (this.currentBytes >= this.maxBytes);
+    }
+
+    changeFileStatus(status){
+        this.fileFlag = (status==='write')?'a+':'r';
+        this.mode = status;
     }
 }
 
