7059e4760f9e185fe3814f381a96ac4a4698cea9
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-20 19:41:07 +0000

    wrote a tree lookup for segment files

---
+++ src/utils/binarySearch.js
@@ -0,0 +1,21 @@
+const upperBound = (arr, target)=>{
+    let low = 0;
+    let high = arr.length-1;
+    let floorIndex = 0;
+
+    while(low<=high){
+        const mid = (low+high)>>1;
+        const currentOffset = arr[mid];
+
+        if(currentOffset == target) return currentOffset;
+        else if(currentOffset<target){
+            floorIndex = mid;
+            low = mid+1;
+        }
+        else{
+            high = mid-1;
+        }
+    }
+
+    return arr[floorIndex];
+}
