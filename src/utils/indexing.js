6f9a36763d8d5ac6c8152b4be324327930f35a86
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-20 20:43:48 +0000

    added mmap and sparse indexing

---
+++ src/utils/indexing.js
@@ -0,0 +1,48 @@
+const {lowerBound} = require('./binarySearch');
+const mmap = require('mmap-io');
+
+const NUM_ENTRIES=-1; //placeholder, will decide and change later
+const ENTRY_SIZE=12; 
+
+const TreeMapLookup = (segmentOffsets, targetOffset)=>{ // assuming segment offset to be a list of BigInts
+    return lowerBound(segmentOffsets, targetOffset);
+}
+
+const getRelativeOffset = (baseOffset, targetOffset)=>{
+    return (targetOffset-baseOffset);
+}
+
+const getFileIndex = (indexFd, relativeOffset)=>{
+    const buffer = mmap.map(
+        NUM_ENTRIES*ENTRY_SIZE,
+        mmap.PROT_READ,
+        mmap.MAP_SHARED,
+        indexFd,
+        0
+    );
+
+    let low = 0;
+    let high = NUM_ENTRIES-1; //buffer.size gives bytes, you absolute spongehead :)
+
+    let best = -1;
+
+    while(low<=high){
+        const mid = (low+high)>>1;
+        const offsetMid = ENTRY_SIZE*mid;
+        const key = buffer.readBigInt64BE(offsetMid);
+
+        if(key==relativeOffset) return buffer.readUInt32BE(offsetMid + 8);
+        else if (key < relativeOffset) {
+            best = mid; 
+            low = mid + 1;
+        } 
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (best === -1) return -1;
+
+    let fileOffset = best*ENTRY_SIZE + 8;
+    return buffer.readUInt32BE(fileOffset);
+}
