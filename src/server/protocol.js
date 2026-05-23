ba1bf223495119c7bfea227fd55e080244f52ab9
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-23 18:49:59 +0000

    added parsing

--- src/server/protocol.js
+++ src/server/protocol.js
@@ -1,10 +1,9 @@
 // [frame_length (4B)][reqCode (1B)][user_id_length (4B)][user_id (variable)][topic_length (4B)][topic_name (variable)][number of messages (4B)][[message_size, message]]
 
-
 const wrapMessage = (userId, topicName, messages, reqCode) => {
     const userIdByteLength = Buffer.byteLength(userId, 'utf-8');
     const topicByteLength = Buffer.byteLength(topicName, 'utf-8');
-    
+
     const messageLengths = messages.map(m => Buffer.byteLength(m, 'utf-8'));
     const totalMsgBytes = messageLengths.reduce((sum, l) => sum + 4 + l, 0);
 
@@ -47,6 +46,47 @@ const wrapMessage = (userId, topicName, messages, reqCode) => {
     return frame.subarray(0, cursor);
 }
 
+const parseMessage = (buffer)=>{
+    let cursor = 4;
+
+    const reqCode = buffer.readUInt8(cursor);
+    cursor+=1;
+
+    const userIdByteLength = buffer.readUInt32BE(cursor);
+    cursor += 4;
+
+    const userId = buffer.toString('utf-8', cursor, cursor+userIdByteLength);
+    cursor+= userIdByteLength;
+
+    const topicByteLength = buffer.readUInt32BE(cursor);
+    cursor+=4;
+
+    const topicName = buffer.toString('utf-8', cursor, cursor+topicByteLength);
+    cursor+=topicByteLength;
+
+    const numMessages = buffer.readUInt32BE(cursor);
+    cursor+=4;
+
+    let messages = [];
+
+    for(let i=0; i<numMessages; i++){
+        const msgLength = buffer.readUInt32BE(cursor);
+        cursor+=4;
+
+        const msg = buffer.toString('utf-8', cursor, cursor+msgLength);
+        messages.push(msg);
+        cursor+= msgLength;
+    }
+
+    return {
+        reqCode: reqCode,
+        userId: userId,
+        topicName: topicName,
+        messages: messages
+    }
+}
+
 module.exports = {
-    wrapMessage
+    wrapMessage,
+    parseMessage
 }
