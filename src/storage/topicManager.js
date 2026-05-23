6f696a5bfae0d96eb7ddc75025e4760a66d17ea6
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-23 11:25:45 +0000

    created dir with topic manager

--- src/storage/topicManager.js
+++ src/storage/topicManager.js
@@ -1,17 +1,37 @@
 const Topic = require('./topic');
+const path = require('path');
+const fs = require('fs');
 
 class TopicManager{ // I keep one topic manager per user
     constructor(userId){
         this.topics = new Map(); // This contains: topic_name:topic_instance
-        this.userId = userId,
-        this.topicDir = '../data'
+        this.userId = userId;
+        this.topicDir = path.join(__dirname, '../../data', userId);
+
+        this.createManageDir();
     }
 
     createTopic(topicName){
-        const topic = new Topic(this.userId, topicName, this.topicDir);
+        const pathTopic = path.join(this.topicDir, topicName);
+        if(!fs.existsSync(pathTopic)){
+            fs.mkdirSync(pathTopic);
+        }
+        const topic = new Topic(this.userId, topicName, pathTopic);
         this.topics.set(topicName, topic);
     }
 
+    getOrCreate(topicName){
+        if(!this.topics.has(topicName)){
+            this.createTopic(topicName);
+        }
+        return this.topics.get(topicName);
+    }
+
+    createManageDir(){
+        if(!fs.existsSync(this.topicDir)){
+            fs.mkdirSync(this.topicDir);
+        }
+    }
     fetchTopic(topicName){
         return this.topics.get(topicName);
     }
