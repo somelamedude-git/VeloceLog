1c6368d8efd8adb463d69f7047d7873e756b322e
somelamedude-git <cozydeveloper843@gmail.com>
2026-05-23 16:20:19 +0000

    made wrapping dynamic

--- src/server/protocol.js
+++ src/server/protocol.js
@@ -1,24 +1,28 @@
-// [frame_length (4B)][reqCode (1B)][user_id (4B)][topic_length (2B)][topic_name (variable size, create boundaries)][number of messages (2B)][[message_size, message]]
+o// [frame_length (4B)][reqCode (1B)][user_id_length (4B)][user_id (variable)][topic_length (4B)][topic_name (variable)][number of messages (4B)][[message_size, message]]
 
 
-const wrapMessage = (userId, topicName, userId, messages, reqCode)=>{
+const wrapMessage = (userId, topicName, messages, reqCode)=>{
     const frame = Buffer.allocUnsafe(4096);
     let cursor = 4;
     frame.writeUint8(reqCode, cursor);
     cursor += 1;
-    frame.writeUInt32BE(userId, cursor);
-    cursor+=4;
+
+    const userIdByteLength = Buffer.byteLength(userId, 'utf-8');
+    frame.writeUInt32BE(userIdByteLength, cursor);
+    cursor += 4;
+    frame.write(userId, cursor, userIdByteLength, 'utf-8');
+    cursor += userIdByteLength;
 
     const topicByteLength = Buffer.byteLength(topicName, 'utf-8');
-    frame.writeUint16BE(topicByteLength, cursor);
-    cursor +=2;
+    frame.writeUInt32BE(topicByteLength, cursor);
+    cursor += 4;
 
     frame.write(topicName, cursor, topicByteLength, 'utf-8');
     cursor += topicByteLength;
 
     const len = messages.length;
-    frame.writeUint16BE(len, cursor);
-    cursor+=2;
+    frame.writeUInt32BE(len, cursor);
+    cursor += 4;
 
     for(const msg of messages){
         const msgLen = Buffer.byteLength(msg, 'utf-8');
@@ -26,10 +30,14 @@ const wrapMessage = (userId, topicName, userId, messages, reqCode)=>{
         cursor += 4;
 
         frame.write(msg, cursor, msgLen, 'utf-8');
-        cursor+=msgLen;
+        cursor += msgLen;
     }
 
     frame.writeUInt32BE(cursor, 0);
 
     return frame.subarray(0, cursor);
+}
+
+module.exports = {
+    wrapMessage
 }
